9b404f1c41d314d729ffb489a00cfb63830e14a0
Codiemaureen <codiestover.cs@gmail.com>
2025-06-06 12:18:07 -0400

    Adding constant variables

---
+++ lib/constants/index.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Prostore';
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'A Modern ecommerce store built with next'
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000/'
