d9a2018ea9f2caf45a11399c97b7dfc0c18ba2d1
Codiemaureen <codiestover.cs@gmail.com>
2025-06-08 15:42:15 -0400

    Adding sign in and cart to header

--- app/components/shared/header/index.tsx
+++ app/components/shared/header/index.tsx
@@ -1,22 +1,40 @@
 import { ShoppingCart, UserIcon } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
-import Button from '../../ui/button';
+import { Button } from '../../ui/button';
 import { APP_NAME } from '@/lib/constants';
 import logo from '@/public/logo.svg'
 
 const Header = () => {
- return ( <header className='w-full border-b'>
-   <div className="wrapper flex-between">
-    <div className="flex-start">
-     <Link href={`/`} className='flex-start'>
-      <Image src={logo} alt='logo' width={40} height={40}/>
-     </Link>
-     <span className="hidden font-bold text-2xl ml-3">{APP_NAME}</span>
-     <Button />
+  return ( 
+  <header className='w-full border-b'>
+    <div className="wrapper flex-between">
+      <div className="flex-start">
+      <Link href={`/`} className='flex-start'>
+        <Image 
+          src={logo} 
+          alt={`${APP_NAME} logo`} 
+          width={40} 
+          height={40}/>
+        <span className="hidden lg:block font-bold text-2xl ml-3">
+          {APP_NAME}
+        </span>
+      </Link>
+      </div>
+      <div className="space-x-2">
+        <Button asChild variant='ghost'>
+          <Link href='/sign-in'>
+            <UserIcon /> Sign in
+          </Link>
+        </Button>
+        <Button asChild variant='ghost'>
+          <Link href='/sign-in'>
+            <ShoppingCart /> Cart
+          </Link>
+        </Button>
+      </div>
     </div>
-   </div>
- </header> );
+  </header> );
 }
- 
+
 export default Header;
