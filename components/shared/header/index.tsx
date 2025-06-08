8fbc140b9fe2f4c856af32f447c8ea7dcb8b6f23
Codiemaureen <codiestover.cs@gmail.com>
2025-06-08 18:54:27 -0400

    Adding system theme toggle

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { Button } from '../../ui/button';
 import { APP_NAME } from '@/lib/constants';
 import logo from '@/public/logo.svg'
+import ModeToggle from './mode-toggle';
 
 const Header = () => {
   return ( 
@@ -22,6 +23,7 @@ const Header = () => {
       </Link>
       </div>
       <div className="space-x-2">
+        <ModeToggle />
         <Button asChild variant='ghost'>
           <Link href='/cart'>
             <ShoppingCart /> Cart
