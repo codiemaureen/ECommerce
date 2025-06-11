f8d7ea363a37efc927e2d8f17f749233a53a3946
Codiemaureen <codiestover.cs@gmail.com>
2025-06-11 12:17:09 -0400

    Adding product type

--- components/product/product-card.tsx
+++ components/product/product-card.tsx
@@ -2,9 +2,10 @@ import Link from "next/link";
 import Image from "next/image";
 import { Card, CardContent, CardHeader } from "../ui/card";
 import ProductPrice from "./product-price";
+import { Product } from "@/types";
 
 
-const ProductCard = ({product}: {product: any}) => {
+const ProductCard = ({product}: {product: Product}) => {
  return ( 
    <Card className="w-full max-w-sm">
     <CardHeader className="p-0 items-center">
