36cd79ec151c5bee5ce23d90937558b875f022a6
Codiemaureen <codiestover.cs@gmail.com>
2025-06-13 10:55:56 -0400

    Adding product details layout

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -1,5 +1,9 @@
 import { getProductBySlug } from "@/lib/action/product.actions";
+import ProductPrice from "@/components/product/product-price";
 import { notFound } from "next/navigation";
+import { Badge } from "@/components/ui/badge";
+import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 const ProductDetailsPage = async(props: {
  params: Promise<{slug: string}>
@@ -9,7 +13,57 @@ const ProductDetailsPage = async(props: {
  const product = await getProductBySlug(slug);
  if(!product) return notFound();
 
- return ( <>{product.name}</> );
+ return ( <>
+  <section>
+   <div className="grid-grid-cols-1 md:grid-cols-5">
+    {/* images column */}
+    <div className="cols-span-2">
+
+    </div>
+    {/* datails column */}
+     <div className="cols-span-2 p-5">
+      <div className="flex-flex-col gap-6">
+       <p>{product.brand} {product.category}</p>
+       <h1 className="h3-bold">{product.name}</h1>
+       <p>{product.rating} of {product.numReviews} Reviews</p>
+       <div className="flex flex-col sm:flex-row sm:items-center gap-3">
+        <ProductPrice 
+         className="w-24 rounded-full bg-green-100 text-green-700 px-5 py-2"
+         value={Number(product.price)}/>
+       </div>
+      </div>
+      <div className="mt-10">
+       <p className="semi-bold">Description</p>
+       <p>{product.description}</p>
+      </div>
+     </div>
+     {/* action column */}
+     <Card>
+      <CardContent
+       className="p-4">
+        <div
+         className="mb-2 flex justify-between">
+          <div>Price</div>
+          <div>
+           <ProductPrice value={Number(product.price)}/>
+          </div>
+         </div>
+         <div
+          className="mb-2 flex justify-between">
+           <div>Status</div>
+           {product.stock > 0 ? (<Badge variant="outline">In Stock</Badge>) : (
+            <Badge variant="destructive">Out of Stock</Badge>)}
+          </div>
+          {product.stock > 0 && (
+           <div className="flex-center">
+            <Button className="w-full">Add To Cart</Button>
+           </div>
+          )}
+      </CardContent>
+     </Card>
+   </div>
+  </section>
+ </> );
 }
  
 export default ProductDetailsPage;
