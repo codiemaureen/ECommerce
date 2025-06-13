ff42bbafd950916dafd414f8ba63938110d5d8bc
Codiemaureen <codiestover.cs@gmail.com>
2025-06-13 10:12:32 -0400

    Adjusting config for later prisma versions

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,18 +1,16 @@
-import { Pool, neonConfig } from '@neondatabase/serverless';
+import { neonConfig } from '@neondatabase/serverless';
 import { PrismaNeon } from '@prisma/adapter-neon';
 import { PrismaClient } from '@prisma/client';
 import ws from 'ws';
 
 // Sets up WebSocket connections, which enables Neon to use WebSocket communication.
 neonConfig.webSocketConstructor = ws;
-const connectionString = `${process.env.DATABASE_URL}`;
 
-// Creates a new connection pool using the provided connection string, allowing multiple concurrent connections.
-//pool is not need with prisma 5+
-// const pool = new Pool({ connectionString });
+// Instantiate the adapter correctly
+//  (pool is automatically handled with PrismaNeon in prisma 6)
+const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL! });
 
-// Instantiates the Prisma adapter using the Neon connection pool to handle the connection between Prisma and Neon.
-const adapter = new PrismaNeon({connectionString});
+// Initialize Prisma Client with the Neon adapter
 
 // Extends the PrismaClient with a custom result transformer to convert the price and rating fields to strings.
 export const prisma = new PrismaClient({ adapter }).$extends({
