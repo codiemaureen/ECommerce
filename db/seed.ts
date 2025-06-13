dd32ab456aca138c4085263b8eda9041ccd61701
Codiemaureen <codiestover.cs@gmail.com>
2025-06-13 13:20:14 -0400

    Fixing build error

--- db/seed.ts
+++ db/seed.ts
@@ -1,8 +1,8 @@
-import { PrismaClient } from '../generated/prisma';
+import { prisma } from '@/db/prisma';
 import sampleData from './sample-data';
 
 async function main() {
-  const prisma = new PrismaClient();
+  // const prisma = new PrismaClient();
   await prisma.product.deleteMany();
 
   await prisma.product.createMany({ data: sampleData.products});
