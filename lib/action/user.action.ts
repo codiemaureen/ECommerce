c83abaa2057cf4618c648935388b9707a2983acf
Codiemaureen <codiestover.cs@gmail.com>
2025-06-23 10:09:23 -0400

    Adding user signup action

--- lib/action/user.action.ts
+++ lib/action/user.action.ts
@@ -1,9 +1,11 @@
 'use server';
 
-import { signInFormSchema } from "../validators";
+import { signInFormSchema, signUpFormSchema } from "../validators";
 import { signIn, signOut } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
 import { AuthActionState } from "@/types";
+import { hashSync } from "bcrypt-ts-edge";
+import { prisma } from "@/db/prisma";
 
 
 // sign in the user with credentials
@@ -26,7 +28,43 @@ export async function signInWithCredentials(prevState: AuthActionState, formData
 }
 
 //sign user out
-
 export async function signOutUser(){
  await signOut();
+}
+
+//sign user up
+export async function signUpUser(prevState: unknown, formData: FormData){
+ try {
+  const user = signUpFormSchema.parse({
+   name: formData.get('name'),
+   email: formData.get('email'),
+   password: formData.get('password'),
+   confirmPassword: formData.get('confirmPassword'),
+  });
+  const unalteredPassword = user.password;
+  user.password = hashSync(user.password, 10);
+
+  await prisma.user.create({
+   data: {
+    name: user.name,
+    email: user.email,
+    password: user.password
+   }
+  });
+
+  await signIn('credentials', {
+   email: user.email,
+   password: unalteredPassword
+  })
+
+  return {
+   success: true,
+   message: 'User registered successfully'
+  }
+ } catch (error) {
+  if(isRedirectError(error)){
+   throw error;
+  }
+  return { success: false, message: 'User not registered' }
+ }
 }
