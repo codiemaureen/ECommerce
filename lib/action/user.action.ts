914a7b84b8a032821f02904bedee417c0ee8f1bd
Codiemaureen <codiestover.cs@gmail.com>
2025-07-02 14:36:59 -0400

    adding update user address function

--- lib/action/user.action.ts
+++ lib/action/user.action.ts
@@ -1,9 +1,9 @@
 'use server';
 
-import { signInFormSchema, signUpFormSchema } from "../validators";
-import { signIn, signOut } from "@/auth";
+import { shippingAddressSchema, signInFormSchema, signUpFormSchema } from "../validators";
+import { auth, signIn, signOut } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
-import { AuthActionState } from "@/types";
+import { AuthActionState, ShippingAddress } from "@/types";
 import { hashSync } from "bcryptjs";
 import { prisma } from "@/db/prisma";
 import { formatError } from "../utils";
@@ -76,4 +76,34 @@ export async function getUserById(userId: string){
  });
  if(!user) throw new Error('User not found');
  return user;
+}
+
+export async function updateUserAddress(data: ShippingAddress){
+ try {
+  const session = await auth();
+
+  const currentUser = prisma.user.findFirst({
+   where: {id: session?.user?.id}
+  });
+
+  if(!currentUser) throw new Error('User not found');
+
+  const address = shippingAddressSchema.parse(data);
+
+  await prisma.user.update({
+   where: {id: (await currentUser).id},
+   data:{address}
+  })
+  
+ } catch (error) {
+  return { 
+   success: false,
+   message: formatError(error)
+  }
+ }
+
+ return {
+  success: true,
+  message: 'User address updated successfully'
+ }
 }
