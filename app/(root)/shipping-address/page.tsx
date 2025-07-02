bedf65277129a3a6789fe40ab3e1ce8c3b3287f1
Codiemaureen <codiestover.cs@gmail.com>
2025-07-02 11:11:27 -0400

    Getting cart and user

---
+++ app/(root)/shipping-address/page.tsx
@@ -0,0 +1,35 @@
+import { auth } from '@/auth';
+import { getMyCart } from '@/lib/action/cart.actions';
+import { getUserById } from '@/lib/action/user.action';
+import { Metadata } from 'next';
+import { redirect } from 'next/navigation';
+import { ShippingAddress } from '@/types';
+import { cookies } from "next/headers";
+
+export const metadata: Metadata = {
+ title: 'Shipping Address'
+}
+
+const ShippingAddressPage = async () => {
+ // get cart
+ const cookieStore = await cookies();
+ const sessionCartId = cookieStore.get('sessionCartId')?.value;
+ const cart = await getMyCart(sessionCartId);
+
+ if(!cart || cart.items.length === 0) redirect('/cart');
+
+ const session = await auth();
+
+ const userId = session?.user?.id;
+
+ if(!userId) throw new Error('No user ID');
+
+ const user = await getUserById(userId);
+
+ console.log(user)
+ return ( <>
+  Address
+ </> );
+}
+ 
+export default ShippingAddressPage;
