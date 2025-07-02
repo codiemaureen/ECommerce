330c9445e0a743ebf6c4c2e264a148b1ff0ac094
Codiemaureen <codiestover.cs@gmail.com>
2025-07-02 13:59:28 -0400

    adding shipping address form

--- app/(root)/shipping-address/page.tsx
+++ app/(root)/shipping-address/page.tsx
@@ -5,6 +5,7 @@ import { Metadata } from 'next';
 import { redirect } from 'next/navigation';
 import { ShippingAddress } from '@/types';
 import { cookies } from "next/headers";
+import ShippingAddressForm from './shipping-address-form';
 
 export const metadata: Metadata = {
  title: 'Shipping Address'
@@ -26,9 +27,8 @@ const ShippingAddressPage = async () => {
 
  const user = await getUserById(userId);
 
- console.log(user)
  return ( <>
-  Address
+  <ShippingAddressForm address={user.address as ShippingAddress}/>
  </> );
 }
  
---
+++ app/(root)/shipping-address/shipping-address-form.tsx
@@ -0,0 +1,121 @@
+'use client';
+
+import { ShippingAddress } from "@/types";
+import { shippingAddressSchema } from "@/lib/validators";
+import { useRouter } from "next/navigation";
+import { useTransition } from "react";
+import { toast } from "sonner";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ControllerRenderProps, useForm } from "react-hook-form";
+import { z } from "zod";
+import { shippingAddressDefaultValues } from "@/lib/constants";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage, } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { ArrowRight, Loader } from "lucide-react";
+
+const ShippingAddressForm = ({address}: {address: ShippingAddress}) => {
+ const router =  useRouter();
+
+ const form = useForm<z.infer<typeof shippingAddressSchema>>({
+  resolver: zodResolver(shippingAddressSchema),
+  defaultValues: address || shippingAddressDefaultValues,
+ })
+
+ const [isPending, startTransition] =  useTransition();
+ const onSubmit = () =>{
+  return
+ }
+ 
+ return ( 
+ <>
+  <div className="max-w-md mx-auto space-y-4">
+   <h1 className="h2-bold mt-4">
+    Shipping Address
+   </h1>
+   <p className="text-sm text-muted-forground">
+    Please enter your shipping address
+   </p>
+   <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+         control={form.control}
+          name="fullName"
+          render={({ field }: {field: ControllerRenderProps<z.infer<typeof shippingAddressSchema>, 'fullName'>}) => (
+            <FormItem>
+              <FormLabel>Full Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter full name" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+         control={form.control}
+          name="streetAddress"
+          render={({ field }: {field: ControllerRenderProps<z.infer<typeof shippingAddressSchema>, 'streetAddress'>}) => (
+            <FormItem>
+              <FormLabel>Street Address</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter street address" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+         control={form.control}
+          name="city"
+          render={({ field }: {field: ControllerRenderProps<z.infer<typeof shippingAddressSchema>, 'city'>}) => (
+            <FormItem>
+              <FormLabel>City</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter City" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+         control={form.control}
+          name="postalCode"
+          render={({ field }: {field: ControllerRenderProps<z.infer<typeof shippingAddressSchema>, 'postalCode'>}) => (
+            <FormItem>
+              <FormLabel>Postal Code</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter Zip Code" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+         control={form.control}
+          name="country"
+          render={({ field }: {field: ControllerRenderProps<z.infer<typeof shippingAddressSchema>, 'country'>}) => (
+            <FormItem>
+              <FormLabel>Postal Code</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter Country" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="flex gap-2">
+         <Button 
+          type="submit" 
+          disabled={isPending}>
+           {isPending 
+            ? (<Loader animate-spin='true/' className="w-4 h-4"/>)
+            : (<ArrowRight className="h-4 w-4" />)} Continue Checkout
+          </Button>
+        </div>
+      </form>
+    </Form>
+  </div>
+ </> );
+}
+ 
+export default ShippingAddressForm;
