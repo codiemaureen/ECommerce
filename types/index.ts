54761abb0243e74ec33944a6cadd747e1878ea8d
Codiemaureen <codiestover.cs@gmail.com>
2025-07-02 09:44:12 -0400

    Creating shipping address type

--- types/index.ts
+++ types/index.ts
@@ -1,7 +1,8 @@
 import { z } from "zod";
 import { insertProductSchema, 
          insertCartSchema, 
-         cartItemSchema } from "@/lib/validators";
+         cartItemSchema, 
+         shippingAddressSchema} from "@/lib/validators";
 
 
 export type AuthActionState = {
@@ -19,3 +20,5 @@ export type Product = z.infer<typeof insertProductSchema> & {
 export type Cart = z.infer<typeof insertCartSchema>;
 
 export type CartItem = z.infer<typeof cartItemSchema>;
+
+export type ShippingAddress = z.infer<typeof shippingAddressSchema>
