75f01d793768aa13debff7191630c6daa142f484
Codiemaureen <codiestover.cs@gmail.com>
2025-07-02 14:37:33 -0400

    Removning placeholder valuse

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -15,9 +15,9 @@ export const signUpDefaultValues = {
 }
 
 export const shippingAddressDefaultValues = {
- fullName: 'John Doe',
- streetAddress: '123 main street',
- city: 'Anytown',
- postalCode: '12345',
- country: 'USA',
+ fullName: '',
+ streetAddress: '',
+ city: '',
+ postalCode: '',
+ country: '',
 }
