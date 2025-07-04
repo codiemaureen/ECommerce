e33c04c3f62e85ecde12f966e34b1ac0a6721918
Codiemaureen <codiestover.cs@gmail.com>
2025-07-04 14:50:21 -0400

    Adding validators and types

--- types/index.ts
+++ types/index.ts
@@ -2,7 +2,9 @@ import { z } from "zod";
 import { insertProductSchema, 
          insertCartSchema, 
          cartItemSchema, 
-         shippingAddressSchema} from "@/lib/validators";
+         shippingAddressSchema,
+         insertOrderItemSchema,
+         insertOrderSchema} from "@/lib/validators";
 
 
 export type AuthActionState = {
@@ -18,7 +20,7 @@ export type Product = z.infer<typeof insertProductSchema> & {
 }
 
 export type Cart = z.infer<typeof insertCartSchema>;
-
 export type CartItem = z.infer<typeof cartItemSchema>;
-
 export type ShippingAddress = z.infer<typeof shippingAddressSchema>
+export type OrderItem = z.infer<typeof insertOrderItemSchema>
+export type Order = z.infer<typeof insertOrderSchema>
