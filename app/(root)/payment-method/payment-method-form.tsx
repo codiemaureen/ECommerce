95abe36be4d671fcbd347dce78c364bc320fa512
Codiemaureen <codiestover.cs@gmail.com>
2025-07-04 10:44:53 -0400

    Creating payment method page

--- app/(root)/payment-method/payment-method-form.tsx
+++ app/(root)/payment-method/payment-method-form.tsx
@@ -3,12 +3,16 @@
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 import { useTransition } from "react";
-import { Form } from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
 import { paymentMethodSchema } from "@/lib/validators";
 import z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { DEFAULT_PAYMENT_METHOD } from "@/lib/constants";
+import { DEFAULT_PAYMENT_METHODS, PAYMENT_METHODS } from "@/lib/constants";
 import { useForm } from "react-hook-form";
+import { ArrowRight, Loader } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { updateUserPaymentMethod } from "@/lib/action/user.action";
 
 const PaymentMethodForm = ({ preferredPaymentMethod }: {preferredPaymentMethod: string | null}) => {
  const router = useRouter();
@@ -16,14 +20,76 @@ const PaymentMethodForm = ({ preferredPaymentMethod }: {preferredPaymentMethod: 
  const form = useForm<z.infer<typeof paymentMethodSchema>>({
   resolver: zodResolver(paymentMethodSchema),
   defaultValues: {
-   type: preferredPaymentMethod || DEFAULT_PAYMENT_METHOD
+   type: preferredPaymentMethod || DEFAULT_PAYMENT_METHODS
   }
  })
  const [isPending, startTransition] = useTransition();
 
- return ( <>
-  Form
- </> );
+ const onSubmit = async (values: z.infer<typeof paymentMethodSchema>) => {
+  startTransition(async () => {
+   const res = await updateUserPaymentMethod(values);
+
+   if(!res.success){
+    toast.error(res.message)
+   }
+   router.push('/place-order');
+  })
+ };
+
+ return ( 
+  <>
+  <div className="max-w-md mx-auto space-y-4">
+  <h1 className="h2-bold mt-4">
+    Payment Method
+  </h1>
+  <p className="text-sm text-muted-forground">
+    Please select the Payment Method
+  </p>
+  <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+       <div className="flex flex-col md:flex-row gap-5">
+        <FormField
+         control={form.control}
+         name='type'
+         render={({field}) => (
+          <FormItem className="space-y-3">
+           <FormControl>
+            <RadioGroup 
+             onValueChange={field.onChange}
+             className="flex flex-col space-y-2">
+              {PAYMENT_METHODS.map((paymentMethod) => (
+               <FormItem key={paymentMethod} 
+                className="flex items-center space-x-3 space-y-0">
+                 <FormControl>
+                  <RadioGroupItem 
+                   value={paymentMethod} 
+                   checked={field.value === paymentMethod}/>
+                 </FormControl>
+                 <FormLabel className="font-normal">
+                  {paymentMethod}
+                 </FormLabel>
+                </FormItem>
+              ))}
+            </RadioGroup>
+           </FormControl>
+          </FormItem>
+         )}/>
+       </div>
+       <div className="flex gap-2">
+        <Button 
+          type="submit" 
+          disabled={isPending}
+          className="cursor-pointer">
+          {isPending 
+           ? (<Loader animate-spin='true' className="w-4 h-4"/>)
+           : (<ArrowRight className="h-4 w-4" />)} Continue Checkout
+         </Button>
+       </div>
+      </form>
+    </Form>
+  </div>
+</>
+ );
 }
  
 export default PaymentMethodForm;
