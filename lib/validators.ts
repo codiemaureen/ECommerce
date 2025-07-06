33ae32eb17c5f939fc939fc76a8676c54287f4b6
Codiemaureen <codiestover.cs@gmail.com>
2025-07-06 18:58:15 -0400

    Fixing file indents

--- lib/validators.ts
+++ lib/validators.ts
@@ -1,10 +1,11 @@
 import { z } from 'zod';
 import { formatNumberWithDecimal } from './utils';
 import { PAYMENT_METHODS } from './constants';
+import { Decimal } from '@prisma/client/runtime/library';
 
 const currency = z
- .string()
- .refine((value) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(Number(value))), 'Price must have exactly two decimal places'
+  .string()
+  .refine((value) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(Number(value))), 'Price must have exactly two decimal places'
 );
 
 
@@ -16,34 +17,34 @@ const decimalLike = z.union([
 
 //schema for inserting products
 export const insertProductSchema = z.object({
- name: z.string().min(3, 'Name must be at least 3 characters'),
- slug: z.string().min(3, 'Slug must be at least 3 characters'),
- category: z.string().min(3, 'Category must be at least 3 characters'),
- brand: z.string().min(3, 'Brand must be at least 3 characters'),
- description: z.string().min(3, 'Description must be atleast 3 characters'),
- stock: z.coerce.number(),
- images: z.array(z.string().min(1, "Product must have at least one image")),
- isFeatured: z.boolean(),
- banner: z.string().nullable(),
- price: currency
+  name: z.string().min(3, 'Name must be at least 3 characters'),
+  slug: z.string().min(3, 'Slug must be at least 3 characters'),
+  category: z.string().min(3, 'Category must be at least 3 characters'),
+  brand: z.string().min(3, 'Brand must be at least 3 characters'),
+  description: z.string().min(3, 'Description must be atleast 3 characters'),
+  stock: z.coerce.number(),
+  images: z.array(z.string().min(1, "Product must have at least one image")),
+  isFeatured: z.boolean(),
+  banner: z.string().nullable(),
+  price: currency
 });
 
 // schema for signing users in
 export const signInFormSchema = z.object({
- email: z.string().email('Invalid Email Address'),
- password: z.string().min(6, 'Password must be atleast 6 characters')
+  email: z.string().email('Invalid Email Address'),
+  password: z.string().min(6, 'Password must be atleast 6 characters')
 })
 // schema for signing users up
 
 export const signUpFormSchema = z.object({
- name: z.string().min(3, 'Name must be at least 3 characters'),
- email: z.string().email('Invalid Email Address'),
- password: z.string().min(6, 'Password must be atleast 6 characters'),
- confirmPassword: z.string().min(6, 'Confirm password must be at least 6 characters')
-}).refine((data) => 
- data.password === data.confirmPassword, {
-  message: "Password must match",
-  path: ['confirmPassword']
+  name: z.string().min(3, 'Name must be at least 3 characters'),
+  email: z.string().email('Invalid Email Address'),
+  password: z.string().min(6, 'Password must be atleast 6 characters'),
+  confirmPassword: z.string().min(6, 'Confirm password must be at least 6 characters')
+  }).refine((data) => 
+  data.password === data.confirmPassword, {
+    message: "Password must match",
+    path: ['confirmPassword']
 })
 
 
