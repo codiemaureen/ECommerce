eae9f729298b2f8115d6a57e361cf20181fc7bfb
Codiemaureen <codiestover.cs@gmail.com>
2025-07-10 18:56:19 -0400

    Adding details table to order details page

--- app/(root)/order/[id]/page.tsx
+++ app/(root)/order/[id]/page.tsx
@@ -1,26 +1,32 @@
 import { getOrderById } from "@/lib/action/order.actions";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
+import OrderDetailsTable from "./order-details-table";
+import { DisplayOrder } from "@/types";
+
+
 
 export const metadata: Metadata = {
- title: 'Order Details'
+  title: 'Order Details'
 }
 
 const OrderDetailPage = async (props: {
- params: Promise<{
-  id:string
- }>
-}) => {
- const { id } = await props.params;
- 
- const order = await getOrderById(id);
- if(!order) notFound();
+  params: Promise<{
+    id:string
+  }>
+  }) => {
 
- return ( 
-  <>
-   Details {order.totalPrice}
-  </>
-  );
+  const { id } = await props.params;
+  
+  const order = await getOrderById(id);
+  console.log(order);
+  if(!order) notFound();
+
+  return ( 
+    <>
+      <OrderDetailsTable order={order as DisplayOrder} />
+    </>
+    );
 }
- 
+
 export default OrderDetailPage;
