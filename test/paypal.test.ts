13fec1b3feadb8deb5754d4eb65f467a177b6e4f
Codiemaureen <codiestover.cs@gmail.com>
2025-07-10 20:41:19 -0400

    Adding paypal token test

---
+++ test/paypal.test.ts
@@ -0,0 +1,10 @@
+import { generateAccessToken } from "../lib/paypal";
+
+// test to generate access token from paypal
+
+test('generates a token from paypal', async() => {
+ const tokenResponse = await generateAccessToken();
+ console.log(tokenResponse)
+ expect(typeof tokenResponse).toBe('string');
+ expect(tokenResponse.length).toBeGreaterThan(0);
+})
