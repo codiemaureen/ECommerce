b32f91004c8849e37a761954ed4396ce35e63f5b
Codiemaureen <codiestover.cs@gmail.com>
2025-07-22 20:34:20 -0400

    initialzing main nav for

--- app/user/layout.tsx
+++ app/user/layout.tsx
@@ -2,6 +2,7 @@ import Menu from "@/components/shared/header/menu";
 import { APP_NAME } from "@/lib/constants";
 import Image from "next/image";
 import Link from "next/link";
+import MainNav from "./main-nav";
 
 
 export default function UserLayout({
@@ -17,7 +18,7 @@ export default function UserLayout({
      <Link href='/' className="w-22">
       <Image src='/images/logo.svg' width={48} height={48} alt={APP_NAME}/>
      </Link>
-     {/* main nav */}
+     <MainNav className="mx-6" />
      <div className="ml-auto items-center flex space-x-4">
       <Menu />
      </div>
