00675ea16f7d4b069674c7bec78dd755f10ae255
Codiemaureen <codiestover.cs@gmail.com>
2025-07-23 12:17:07 -0400

    Adding page_size constant

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -25,3 +25,5 @@ export const shippingAddressDefaultValues = {
 export const PAYMENT_METHODS = process.env.PAYMENT_METHODS ? process.env.PAYMENT_METHODS.split(', ') : ['PayPal', 'Stripe', 'CashOnDelivery'];
 
 export const DEFAULT_PAYMENT_METHODS = process.env.DEFAULT_PAYMENT_METHODS || 'PayPal';
+
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE) || 2;
