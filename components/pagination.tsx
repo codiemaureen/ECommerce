a7ef642a1f5ed650c4b00273a280526bc66f6574
Codiemaureen <codiestover.cs@gmail.com>
2025-07-24 20:25:21 -0400

    Adding button click function

--- components/pagination.tsx
+++ components/pagination.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useRouter, useSearchParams } from "next/navigation";
 import { Button } from "./ui/button";
+import { formUrlQuery } from "@/lib/utils";
 
 type PaginationProps = {
  page: number | string;
@@ -8,16 +9,37 @@ type PaginationProps = {
  urlParamName?: string;
 }
 
-const Pagination = ({page, totalPages, UrlParamName}: PaginationProps) => {
+const Pagination = ({page, totalPages, urlParamName}: PaginationProps) => {
  const router =useRouter();
  const searchParams = useSearchParams();
 
+ const handleClick = (btnType: string) => {
+  const pageValue = btnType === 'next' ? Number(page) + 1 : Number(page) - 1;
+  const newUrl = formUrlQuery({
+   params: searchParams.toString(),
+   key: urlParamName || 'page',
+   value: pageValue.toString()
+  })
+  router.push(newUrl);
+ }
+
  return ( 
   <div className="flex gap-2">
-   <Button size="lg" variant="outline" className="w-28" disabled={Number(page) <= 1}>
+   <Button 
+    size="lg" 
+    variant="outline" 
+    className="w-28" 
+    disabled={Number(page) <= 1}
+    onClick={() => handleClick('prev')}>
     Previous
    </Button>
-   <Button size="lg" variant="outline" className="w-28" disabled={Number(page) >= totalPages}>
+
+   <Button 
+    size="lg" 
+    variant="outline" 
+    className="w-28" 
+    disabled={Number(page) >= totalPages}
+    onClick={() => handleClick('next')}>
     Next
    </Button>
   </div>
