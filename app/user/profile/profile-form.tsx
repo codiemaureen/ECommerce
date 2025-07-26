09e19c5877de9dd0a1d80d6432a045ed03005bbc
Codiemaureen <codiestover.cs@gmail.com>
2025-07-26 16:25:41 -0400

    Code clean up

--- app/user/profile/profile-form.tsx
+++ app/user/profile/profile-form.tsx
@@ -13,66 +13,66 @@ import { updateProfile } from "@/lib/action/user.action";
 
 
 const ProfileForm = () => {
- const {data: session, update} = useSession();
- const form = useForm<z.infer<typeof updateProfileSchema>>({
-  resolver: zodResolver(updateProfileSchema),
-  defaultValues: {
-   name: session?.user?.name?? '',
-   email: session?.user?.email?? '',
+  const {data: session, update} = useSession();
+  const form = useForm<z.infer<typeof updateProfileSchema>>({
+    resolver: zodResolver(updateProfileSchema),
+    defaultValues: {
+    name: session?.user?.name?? '',
+    email: session?.user?.email?? '',
 
+    }
+  });
+
+  const onSubmit = async (values: z.infer<typeof updateProfileSchema>) => {
+    const res = await updateProfile(values);
+
+    if(!res.success){
+    return toast.error(res.message);
+    }
+    
+    const newSession = {
+    ...session,
+    user: {
+      ...session.user,
+      name: values.name
+    }
+    }
+
+    await update(newSession);
+    await getSession();
+    toast.success(res.message);
   }
- });
-
- const onSubmit = async (values: z.infer<typeof updateProfileSchema>) => {
-  const res = await updateProfile(values);
-
-  if(!res.success){
-   return toast.error(res.message);
-  }
-  
-  const newSession = {
-   ...session,
-   user: {
-    ...session.user,
-    name: values.name
-   }
-  }
-
-  await update(newSession);
-  await getSession();
-  toast.success(res.message);
- }
- return ( <Form {...form}>
-  <form className="flex flex-col gap-5" onSubmit={form.handleSubmit(onSubmit)}>
-   <div className="flex flex-col gap-5">
-    <FormField 
-     control={form.control}
-     name="email"
-     render={({ field }) => (
-      <FormItem className="w-full">
-       <FormControl>
-        <Input disabled placeholder="Email" className="input-field" {...field} />
-       </FormControl>
-      </FormItem>
-    )}
-    ></FormField>
-    <FormField 
-     control={form.control}
-     name="name"
-     render={({ field }) => (
-      <FormItem className="w-full">
-       <FormControl>
-        <Input placeholder="Name" className="input-field" {...field} />
-       </FormControl>
-      </FormItem>
-    )}
-    ></FormField>
-   </div>
-   <Button type='submit' size="lg" className="button col-span-2 w-full" disabled={form.formState.isSubmitting}>
-    {form.formState.isSubmitting ? 'Submitting...' : "Update Profile"}
-   </Button>
-  </form>
- </Form> );
+  return ( <Form {...form}>
+    <form className="flex flex-col gap-5" onSubmit={form.handleSubmit(onSubmit)}>
+    <div className="flex flex-col gap-5">
+      <FormField 
+      control={form.control}
+      name="email"
+      render={({ field }) => (
+        <FormItem className="w-full">
+        <FormControl>
+          <Input disabled placeholder="Email" className="input-field" {...field} />
+        </FormControl>
+        </FormItem>
+      )}
+      ></FormField>
+      <FormField 
+      control={form.control}
+      name="name"
+      render={({ field }) => (
+        <FormItem className="w-full">
+        <FormControl>
+          <Input placeholder="Name" className="input-field" {...field} />
+        </FormControl>
+        </FormItem>
+      )}
+      ></FormField>
+    </div>
+    <Button type='submit' size="lg" className="button col-span-2 w-full" disabled={form.formState.isSubmitting}>
+      {form.formState.isSubmitting ? 'Submitting...' : "Update Profile"}
+    </Button>
+    </form>
+  </Form> );
 }
- 
+
 export default ProfileForm;
