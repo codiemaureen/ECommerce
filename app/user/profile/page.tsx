528eced2d90a7f1f75c6ba83d64f35dfc0392fd2
Codiemaureen <codiestover.cs@gmail.com>
2025-07-26 18:48:10 -0400

    removing session provider from page, adding to page layout

--- app/user/profile/page.tsx
+++ app/user/profile/page.tsx
@@ -1,7 +1,8 @@
 import { Metadata } from "next";
 import { auth } from "@/auth";
-import { SessionProvider } from 'next-auth/react';
+// import { SessionProvider } from 'next-auth/react';
 import ProfileForm from "./profile-form";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: 'Customer Profile'
@@ -9,13 +10,13 @@ export const metadata: Metadata = {
 
 const Profile = async () => {
   const session = await auth();
+  if (!session) redirect('/login');
+  
   return ( 
-    <SessionProvider session={session}>
       <div className="max-w-md mx-auto space-y-4">
         <h2 className="h2-bold">Profile</h2>
         <ProfileForm />
       </div>
-    </SessionProvider>
     );
 }
 
