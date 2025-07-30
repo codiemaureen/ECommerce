f69b42c8a1a23cd9279f1c56a7a6e11c55f1ea2d
Codiemaureen <codiestover.cs@gmail.com>
2025-07-30 08:42:38 -0400

    Typo

--- app/admin/overview/page.tsx
+++ app/admin/overview/page.tsx
@@ -6,7 +6,7 @@ import { formatCurrency, formatDateTime, formatNumber } from "@/lib/utils";
 import { BadgeDollarSign, Barcode, CreditCard, Users } from "lucide-react";
 import { Metadata } from "next";
 import Link from "next/link";
-import { format } from "path";
+
 
 export const metadata: Metadata = {
   title: 'Admin Overview '
