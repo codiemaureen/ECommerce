7e8f70fc149784ee6b71a4dcd970b1d064057dd6
Codiemaureen <codiestover.cs@gmail.com>
2025-08-01 19:14:40 -0400

    Adding isfeatured annd banner

--- components/admin/product-form.tsx
+++ components/admin/product-form.tsx
@@ -17,6 +17,7 @@ import { createProduct, updateProduct } from "@/lib/action/product.actions";
 import { UploadButton } from "@/lib/uploadthing";
 import { Card, CardContent } from "../ui/card";
 import Image from "next/image";
+import { Checkbox } from "../ui/checkbox";
 
 
 const ProductForm = ({type, product, productId}: {
@@ -60,6 +61,8 @@ const ProductForm = ({type, product, productId}: {
   }
 
   const images = form.watch('images');
+  const isFeatured = form.watch('isFeatured');
+  const banner = form.watch('banner');
   
   return ( 
     <Form {...form}>
@@ -187,7 +190,6 @@ const ProductForm = ({type, product, productId}: {
                             toast.error(`ERROR! ${error.message}`)
                           }}
                           />
-
                         </FormControl>
                       </div>
 
@@ -199,7 +201,43 @@ const ProductForm = ({type, product, productId}: {
           />
         </div>
         <div className="upload-field">
-        {/* isfeatured */}
+          {/* isfeatured */}
+          Featured Product
+          <Card>
+            <CardContent className="space-y-2 mt-2">
+              <FormField 
+                control={form.control}
+                name='isFeatured'
+                render={({field}) => (
+                  <FormItem className="flex space-x-2 items-center">
+                    <FormControl>
+                      <Checkbox checked={field.value} onCheckedChange={field.onChange} />
+                    </FormControl>
+                    <FormLabel>Is Featured?</FormLabel>
+                  </FormItem>
+                )}
+              />
+              {isFeatured && banner && (
+                <Image 
+                  src={banner} 
+                  alt="banner image" 
+                  className="w-full object-cover object-center rounded-sm" 
+                  width={1920}
+                  height={680}/>
+                  
+              )}
+
+              {isFeatured && !banner && (
+                <UploadButton endpoint='imageUploader' onClientUploadComplete={(res: {url: string}[]) => {
+                  form.setValue('banner', res[0].url)
+                }}
+                onUploadError={(error: Error) => {
+                  toast.error(`ERROR! ${error.message}`)
+                }}
+                />
+              )}
+            </CardContent>
+          </Card>
         </div>
         <div>
         {/* description */}
