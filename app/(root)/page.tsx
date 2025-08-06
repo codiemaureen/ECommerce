ea72262ba35295ac65e5e071e6dbc2c165b76311
Codiemaureen <codiestover.cs@gmail.com>
2025-08-06 11:07:51 -0400

    Adding carousel to UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,15 @@
-import { getLatestProducts } from "@/lib/action/product.actions";
+import { getLatestProducts, getFeaturedProducts } from "@/lib/action/product.actions";
 import ProductList from "@/components/shared/product/product-list";
+import ProductCarousel from "@/components/shared/product/product.carousel";
+
 
 const HomePage = async () => {
   const latestProducts = await getLatestProducts();
+  const featuredProducts = await getFeaturedProducts();
 
   return ( 
     <div>
+      {featuredProducts.length > 0 && <ProductCarousel data={featuredProducts}/>}
       <ProductList data={latestProducts} title='Newest Arrivals' limit={4}/>
     </div>
   );
