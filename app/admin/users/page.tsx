9e0b2ad63d200203682b8ba682bd8ebd1c4b5c21
Codiemaureen <codiestover.cs@gmail.com>
2025-08-05 20:48:07 -0400

    Adding remove filter to UI

--- app/admin/users/page.tsx
+++ app/admin/users/page.tsx
@@ -15,15 +15,26 @@ export const metadata: Metadata = {
 const AdminUserPage = async (props: {
   searchParams: Promise<{
     page: string;
+    query: string
   }>
   }) => {
-  const {page = 1} = await props.searchParams;
-  const users = await getAllUsers({ page: Number(page)});
+  const {page = 1, query:searchText} = await props.searchParams;
+  const users = await getAllUsers({ page: Number(page), query: searchText});
   return ( 
     <div className="space-y-2">
       <h2 className="h2-bold">
         Orders
       </h2>
+      {searchText && (
+        <div>
+          Filtered by <i>&quot;{searchText}&quot;</i>{' '}
+          <Link href='/admin/orders'>
+            <Button variant='outline' size='sm'>
+              Remove Filter
+            </Button>
+          </Link>
+        </div>
+      )}
       <div className="overflow-x-auto">
         <Table>
           <TableHeader>
