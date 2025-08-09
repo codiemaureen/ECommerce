8484258e8bfc27093af70982a46956dfcf70557a
Codiemaureen <codiestover.cs@gmail.com>
2025-08-09 14:40:01 -0400

    Submitting reviews to database

--- app/(root)/product/[slug]/review-form.tsx
+++ app/(root)/product/[slug]/review-form.tsx
@@ -10,101 +10,116 @@ import { insertReviewSchema } from "@/lib/validators";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { StarIcon } from "lucide-react";
 import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 import {toast} from 'sonner'; 
 import z from "zod";
+import { createUpdateReview } from "@/lib/action/review.actions";
 
 const ReviewForm = ({userId, productId, onReviewSubmitted}:{
- userId: string, 
- productId: string, 
- onReviewSubmitted?: () => void
+  userId: string, 
+  productId: string, 
+  onReviewSubmitted: () => void
 }) => {
- const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
- const form = useForm<z.infer<typeof insertReviewSchema>>({
-  resolver: zodResolver(insertReviewSchema),
-  defaultValues: reviewFormDefaultValues
- });
+  const form = useForm<z.infer<typeof insertReviewSchema>>({
+    resolver: zodResolver(insertReviewSchema),
+    defaultValues: reviewFormDefaultValues
+  });
 
- const handleOpenform = () => {
-  setOpen(true);
- }
+  const handleOpenform = () => {
+    form.setValue('productId', productId);
+    form.setValue('userId', userId);
+    
+    setOpen(true);
+  }
 
- return ( 
-  <Dialog open={open} onOpenChange={setOpen}>
-   <Button onClick={handleOpenform} variant="default">
-    Write a Review
-   </Button>
-   <DialogContent className="sm:max-w-[425px]">
-    <Form {...form}>
-     <form method="POST">
-      <DialogHeader>
-       <DialogTitle>Write a Review</DialogTitle>
-       <DialogDescription>
-        Share your thoughts with other customers
-       </DialogDescription>
-      </DialogHeader>
-      <div className="grid gap-4 py-4">
-       <FormField 
-        control={form.control} 
-        name='title'
-        render={({field}) => (
-         <FormItem>
-          <FormLabel>Title</FormLabel>
-          <FormControl>
-           <Input placeholder="Enter Title" {...field}/>
-          </FormControl>
-         </FormItem>
-        )}
-       />
-       <FormField 
-        control={form.control} 
-        name='description'
-        render={({field}) => (
-         <FormItem>
-          <FormLabel>Description</FormLabel>
-          <FormControl>
-           <Input placeholder="Enter Description" {...field}/>
-          </FormControl>
-         </FormItem>
-        )}
-       />
-       <FormField 
-        control={form.control} 
-        name='rating'
-        render={({field}) => (
-         <FormItem>
-          <FormLabel>Rating</FormLabel>
-          <Select onValueChange={field.onChange} value={field.value.toString()}>
-           <FormControl>
-            <SelectTrigger className="w-full">
-             <SelectValue placeholder="select a rating"/>
-            </SelectTrigger>
-           </FormControl>
-           <SelectContent>
-            {Array.from({length: 5}).map((_, index) => (
-             <SelectItem key={index} value={(index + 1).toString()}>
-              {index + 1} <StarIcon className="inline h-4 w-4"/>
-             </SelectItem>
-             ))}
-           </SelectContent>
-          </Select>
-          <FormMessage />
+  const onSubmit:SubmitHandler<z.infer<typeof insertReviewSchema>> = async (values) => {
+    const res = await createUpdateReview({...values, productId});
 
-         </FormItem>
-        )}
-       />
-      </div>
-      <DialogFooter>
-       <Button type="submit" size="lg" className="w-full" disabled={form.formState.isSubmitting}>
-        {form.formState.isSubmitting ? 'Submitting' : 'Submit'}
-       </Button>
-      </DialogFooter>
-     </form>
-    </Form>
-   </DialogContent>
-  </Dialog>
+    if(!res.success) toast.error(res.message);
+
+    setOpen(false);
+
+    onReviewSubmitted();
+
+    toast.success(res.message);
+  }
+  return ( 
+    <Dialog open={open} onOpenChange={setOpen}>
+    <Button onClick={handleOpenform} variant="default">
+      Write a Review
+    </Button>
+    <DialogContent className="sm:max-w-[425px]">
+      <Form {...form}>
+      <form method="post" onSubmit={form.handleSubmit(onSubmit)}>
+        <DialogHeader>
+        <DialogTitle>Write a Review</DialogTitle>
+        <DialogDescription>
+          Share your thoughts with other customers
+        </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+        <FormField 
+          control={form.control} 
+          name='title'
+          render={({field}) => (
+          <FormItem>
+            <FormLabel>Title</FormLabel>
+            <FormControl>
+            <Input placeholder="Enter Title" {...field}/>
+            </FormControl>
+          </FormItem>
+          )}
+        />
+        <FormField 
+          control={form.control} 
+          name='description'
+          render={({field}) => (
+          <FormItem>
+            <FormLabel>Description</FormLabel>
+            <FormControl>
+            <Input placeholder="Enter Description" {...field}/>
+            </FormControl>
+          </FormItem>
+          )}
+        />
+        <FormField 
+          control={form.control} 
+          name='rating'
+          render={({field}) => (
+          <FormItem>
+            <FormLabel>Rating</FormLabel>
+            <Select onValueChange={field.onChange} value={field.value.toString()}>
+            <FormControl>
+              <SelectTrigger className="w-full">
+              <SelectValue placeholder="select a rating"/>
+              </SelectTrigger>
+            </FormControl>
+            <SelectContent>
+              {Array.from({length: 5}).map((_, index) => (
+              <SelectItem key={index} value={(index + 1).toString()}>
+                {index + 1} <StarIcon className="inline h-4 w-4"/>
+              </SelectItem>
+              ))}
+            </SelectContent>
+            </Select>
+            <FormMessage />
+
+          </FormItem>
+          )}
+        />
+        </div>
+        <DialogFooter>
+        <Button type="submit" size="lg" className="w-full" disabled={form.formState.isSubmitting}>
+          {form.formState.isSubmitting ? 'Submitting' : 'Submit'}
+        </Button>
+        </DialogFooter>
+      </form>
+      </Form>
+    </DialogContent>
+    </Dialog>
   );
 }
- 
+
 export default ReviewForm;
--- app/(root)/product/[slug]/review-list.tsx
+++ app/(root)/product/[slug]/review-list.tsx
@@ -6,25 +6,29 @@ import { useState } from "react";
 import ReviewForm from "./review-form";
 
 const ReviewList = ({userId, productId, productSlug}: {
- userId: string,
- productId: string,
- productSlug: string
+  userId: string,
+  productId: string,
+  productSlug: string
 }) => {
- const [reviews, setReviews] = useState<Review[]>([]);
- return ( 
-  <div className="space-y-4">
-   {reviews.length === 0 && <div>This product has not yet been reviewed</div>}
-   {userId ? 
-     (
-      <ReviewForm userId={userId} productId={productId}/>
-     ) : (
-      <>
-       Please <Link className='text-blue-700 px-2' href={`/sign-in?callbackUrl=/product/${productSlug}`}>Sign In</Link>to write a review
-      </>
-     )
-   }
-  </div> 
- );
+  const [reviews, setReviews] = useState<Review[]>([]);
+
+  const reload = () =>  {
+  console.log(`reloading`);
+  };
+  return ( 
+    <div className="space-y-4">
+    {reviews.length === 0 && <div>This product has not yet been reviewed</div>}
+    {userId ? 
+      (
+        <ReviewForm userId={userId} productId={productId} onReviewSubmitted={reload}/>
+      ) : (
+        <>
+        Please <Link className='text-blue-700 px-2' href={`/sign-in?callbackUrl=/product/${productSlug}`}>Sign In</Link>to write a review
+        </>
+      )
+    }
+    </div> 
+  );
 }
- 
+
 export default ReviewList;
