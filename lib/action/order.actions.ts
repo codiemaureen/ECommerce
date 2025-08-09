317b8c56215fabb5b294e2a810e5ec605c4d0c90
Codiemaureen <codiestover.cs@gmail.com>
2025-08-09 18:49:07 -0400

    exporting updateOrderToPaid for webhooks

--- lib/action/order.actions.ts
+++ lib/action/order.actions.ts
@@ -211,7 +211,7 @@ export async function approvePayPalOrder(orderId: string, data: {orderID: string
 }
 
 // Update order to paid 
-async function updateOrderToPaid({
+export async function updateOrderToPaid({
   orderId,
   paymentResult
 }: {
