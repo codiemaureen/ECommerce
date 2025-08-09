bd0a751b303633164dfcae3ba5873810e9469235
Codiemaureen <codiestover.cs@gmail.com>
2025-08-09 13:44:34 -0400

    Adding review form to UI

--- app/(root)/product/[slug]/review-list.tsx
+++ app/(root)/product/[slug]/review-list.tsx
@@ -3,6 +3,7 @@
 import { Review } from "@/types";
 import Link from "next/link";
 import { useState } from "react";
+import ReviewForm from "./review-form";
 
 const ReviewList = ({userId, productId, productSlug}: {
  userId: string,
@@ -15,9 +16,7 @@ const ReviewList = ({userId, productId, productSlug}: {
    {reviews.length === 0 && <div>This product has not yet been reviewed</div>}
    {userId ? 
      (
-      <>
-       {/* REVIEW FORM HERE */}
-      </>
+      <ReviewForm userId={userId} productId={productId}/>
      ) : (
       <>
        Please <Link className='text-blue-700 px-2' href={`/sign-in?callbackUrl=/product/${productSlug}`}>Sign In</Link>to write a review
