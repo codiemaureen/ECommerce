b61deccb4317e36aac9482e1493b1f05d8b99074
Codiemaureen <codiestover.cs@gmail.com>
2025-08-11 10:51:48 -0400

    Init dealcount down component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ import ProductList from "@/components/shared/product/product-list";
 import ProductCarousel from "@/components/shared/product/product.carousel";
 import ViewAllProductsButton from "@/components/view-all-products-button";
 import IconBoxes from "@/components/icon-boxes";
-
+import DealCountdown from "@/components/deal-countdown";
 
 const HomePage = async () => {
   const latestProducts = await getLatestProducts();
@@ -14,6 +14,7 @@ const HomePage = async () => {
       {featuredProducts.length > 0 && <ProductCarousel data={featuredProducts}/>}
       <ProductList data={latestProducts} title='Newest Arrivals' limit={4}/>
       <ViewAllProductsButton />
+      <DealCountdown />
       <IconBoxes />
     </div>
   );
---
+++ components/deal-countdown.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+const DealCountdown = () => {
+ return ( <>Countdown</> );
+}
+ 
+export default DealCountdown;
